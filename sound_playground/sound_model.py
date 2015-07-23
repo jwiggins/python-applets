addf3fc80be157517a4bc557e852b76801230f19
John Wiggins <jwiggins@enthought.com>
2015-07-23 21:01:38 +0200

    Simplistic conversion of an image to sound

--- sound_playground/sound_model.py
+++ sound_playground/sound_model.py
@@ -1,5 +1,8 @@
+import librosa as lr
 import numpy as np
 from PIL import Image
+from pyaudio import PyAudio
+from skimage import img_as_float
 
 from traits.api import HasStrictTraits, File, Instance
 
@@ -19,14 +22,23 @@ def _is_data_file(filename):
 
 def _load_image(filename):
     img = Image.open(filename)
-    img.draft("L", (256, 256))
+    img.draft("L", (512, 512))
     return np.array(img)
+
+
+def _spec(image):
+    real_image = img_as_float(image)
+    imag_image = 1.0 - real_image
+    comp_image = real_image + 1j * imag_image
+    return lr.istft(comp_image)
 
 
 class SoundModel(HasStrictTraits):
     filename = File()
 
     image_view = Instance(ImageView)
+
+    audio_lib = Instance(PyAudio, args=())
 
     # traits handlers
 
@@ -40,4 +52,10 @@ class SoundModel(HasStrictTraits):
             self.image_view.request_redraw()
 
     def play_sound(self):
-        print "This is a sound"
+        sound = _spec(self.image_view.image)
+        sound =  lr.util.normalize(sound)
+        stream = self.audio_lib.open(format=1, channels=1, rate=11025,
+                                     output=True)
+        stream.write(sound)
+        stream.stop_stream()
+        stream.close()
